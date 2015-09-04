de29c9c91d4f80e0db49a0a535e84a922df73a0d
dianafa <diana.falkowska@gmail.com>
2015-09-04 08:42:09 +0200

    fun with four WebGL

---
+++ assets/js/four_diana.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var floorPattern = new Four.ImageTexture({ path: '/terrain.jpg', anisotropy: 4, minification: Four.gl.LINEAR_MIPMAP_LINEAR, magnification: Four.gl.LINEAR_MIPMAP_LINEAR, mipmap: true });
+var cowLoader = new Four.OBJMeshLoader({ path: '/man.obj' });
+
+function main()
+{
+    var view = new Four.Framebuffer();
+    var program = new Four.Program({ selector: '.diana-shader-class' });
+
+    var camera = new Four.PerspectiveCamera({
+        program: program, location: [50, 50, 50]
+    });
+
+    var light = new Four.Light({
+        program: program,
+        location: [10, 10, 10]
+    });
+
+    var mesh = new Four.Mesh({
+        buffers: new Four.VertexArrayObject({
+            program: program,
+            attributes: ['vec3 position']
+        }),
+        vertices: cowLoader.vertices,
+        uvs: cowLoader.uvs,
+        normals: cowLoader.normals,
+        material: new Four.Material({
+            program: program,
+            diffuse: 0x9F8A60
+        })
+    });
+
+    program.bind();
+
+    spotLight.bind();
+
+    scene = new Four.Scene();
+
+    scene.put(mesh);
+    scene.render(view, camera, function() {
+        program.bind();
+        light.bind();
+
+        scene.rotation += 0.25;
+    });
+}
